6bf4360a39ad7a02591d8fd70557fc23595a0f03
keolecki <kay@autessa.com>
2025-04-05 18:34:38 -0700

    Add wildcard route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { ResumeComponent } from './components/organism/resume/resume.component';
 import { HomeComponent } from './components/organism/home/home.component';
 import { AboutMeComponent } from './components/organism/about-me/about-me.component';
 import { ProjectsComponent } from './components/organism/projects/projects.component';
+import { NotFoundComponent } from './components/organism/not-found/not-found.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'resume', component: ResumeComponent },
     { path: 'about', component: AboutMeComponent },
-    { path: 'projects', component: ProjectsComponent }
+    { path: 'projects', component: ProjectsComponent },
+    { path: '**', component: NotFoundComponent }
 ];
