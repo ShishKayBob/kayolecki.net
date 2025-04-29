87a3c55a23171922e22998bc1ea905de70e9eef8
Kay Olecki <keolecki@gmail.com>
2025-04-29 16:35:43 -0700

    Fixed header/footer and fixedfont pairing

--- resume-site/src/app/app.component.ts
+++ resume-site/src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationBarComponent } from './components/molecule/navigation-bar/navigation-bar.component';
 import { BackgroundComponent } from './components/molecule/background/background.component';
+import { FooterComponent } from './components/molecule/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     NavigationBarComponent,
-    BackgroundComponent
+    BackgroundComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
