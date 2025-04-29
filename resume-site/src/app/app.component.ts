106ebfc38b932bbf227fac9f979e28ee82ac4a1e
Kay Olecki <keolecki@gmail.com>
2025-04-29 16:35:42 -0700

    Created Background

---
+++ resume-site/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavigationBarComponent } from './components/molecule/navigation-bar/navigation-bar.component';
+import { HeroComponent } from './components/molecule/hero/hero.component';
+import { BackgroundComponent } from './components/molecule/background/background.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    NavigationBarComponent,
+    BackgroundComponent,
+    HeroComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+}
